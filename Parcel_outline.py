7876bb7c6e33d3fa60c15c6e80ab8ac901e17a20
jrpaul <jrpaul@users.noreply.github.com>
2020-01-23 21:16:36 -0400

    Update Parcel_outline.py
    Added Output description to script header.

--- Parcel_outline.py
+++ Parcel_outline.py
@@ -5,15 +5,15 @@ Description:
 This scripts generates outlines of parcel polygons. Parcels within 5 meters of each other will be aggregated to create the final polygon.
 
 Input:
-1) A geodatabase holding feature classes
+1) A geodatabase holding feature classes.
 
 Output:
-1) A polygon 
+1) A polygon outline of all merged parcels. The outline polygon has "_outline" in the name.
 
 Version 0.1
 Created by: Juel Paul/Land Analytical
 Date:  January 16, 2020
---------------------------------------------------------------------------------""" 
+--------------------------------------------------------------------------------"""
 
 # Import modules
 import arcpy
@@ -21,7 +21,6 @@ from arcpy import env
 import os
 
 inGDB = arcpy.GetParameterAsText(0) #geodatabase with features
-
 
 # Set workspace and environment variables
 arcpy.env.workspace = inGDB
@@ -41,22 +40,22 @@ completeCount = 0
 for fc in fclist:
     # Outpath for parcel layer copy
     parcelOutline = os.path.join(inGDB, fc + "_" + "outline")
-    
+
     # Check if outline exists, if exists, delete
     if arcpy.Exists(parcelOutline):
         arcpy.AddMessage("{0} exists, not generating outline.".format(fc))
     else:
         # Use aggregate polygons within 5 meters of each other
         arcpy.AggregatePolygons_cartography(fc, parcelOutline, 5)
-        
+
         # Update the progressor label for current feature class
         arcpy.SetProgressorLabel("Merged {0}...".format(fc))
-    
+
         completeCount = completeCount + 1
-        
+
         # Update the progressor position
         arcpy.SetProgressorPosition()
-    
+
         arcpy.AddMessage("{0} outlines complete.".format(completeCount))
 
 arcpy.ResetProgressor()
