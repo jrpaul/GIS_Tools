fbad84f72b7f8404aab5ea6be8093d62618e1df5
jrp <jrpaul@users.noreply.github.com>
2020-04-18 17:27:43 -0400

    Update StandardizeFieldNames_Layouts.py
    Fixed indents and removed list feature class,

--- StandardizeFieldNames_Layouts.py
+++ StandardizeFieldNames_Layouts.py
@@ -42,29 +42,22 @@ namesDict = {
 
 arcpy.AddMessage("Searching the folder now...")
 
-fcList = arcpy.ListFeatureClasses("*", "polygon")
-
-fcCount = len(fcList)
-
-arcpy.AddMessage("{0} layout parcels in this workspace.".format(fcCount))
-
 ftrs = []
-
-# Set the progressor
-arcpy.SetProgressor("step", "Renaming fields...", 0, fcCount, 1)
 
 # Walk GDB and list all polygon features
 for dirpath, dirnames, filenames in arcpy.da.Walk(inGDB, datatype="FeatureClass", type="Polygon"):
     for filename in filenames:
         ftrs.append(os.path.join(dirpath, filename))
+        ftrCount = len(ftrs)
+        arcpy.SetProgressor("step", "Renaming fields...", 0, ftrCount, 1) # Set the progressor
         for ftr in ftrs: # Pass qualified ftrs to list fields
             field_names = [f.name for f in arcpy.ListFields(ftr)]
             for field_name in field_names: # Check if listed fields are in name dict
             	if field_name in namesDict: # If field is in dict, rename field
-            		arcpy.AlterField_management(ftr, field_name, namesDict[field_name])
-            		arcpy.AddMessage("{0} is in {1}. Renaming now...".format(field_name, ftr))
+                    arcpy.AlterField_management(ftr, field_name, namesDict[field_name])
+                    arcpy.AddMessage("{0} is in {1}. Renaming now...".format(field_name, ftr))
                     arcpy.SetProgressorPosition() # Update the progressor position
             	else:
-            		continue
-            		arcpy.AddMessage("{0} is not in {1}.".format(field_name, ftr))
+                    continue
+                    arcpy.AddMessage("{0} is not in {1}.".format(field_name, ftr))
                     arcpy.SetProgressorPosition()
